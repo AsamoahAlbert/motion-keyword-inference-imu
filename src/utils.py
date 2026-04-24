17c1359e2a376bc20354a237256f018395c39144
Albert <111479044+AsamoahAlbert@users.noreply.github.com>
2026-04-24 02:10:40 -0500

    Add utils.py dependency for dataset processing

---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import csv
+import os
+
+def savecsv(filename, savedata):
+    fwrite_data = open(filename, 'w', newline='')
+    writecsv_data = csv.writer(fwrite_data)
+    # writecsv_data.writerow(fields)
+    for i in range(len(savedata)):
+        writecsv_data.writerow(savedata[i])
+    fwrite_data.close()
+
+def readcsv(filename):
+    with open(filename, newline='') as f:
+        reader = csv.reader(f)
+        data_list = list(reader)
+    # label = data_list[0]
+    return data_list
